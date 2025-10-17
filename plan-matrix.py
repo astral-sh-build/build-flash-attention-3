b91039f10d812fa7b9f17097438c4e9e1950dfc8
William Woodruff <william@astral.sh>
2025-10-17 16:50:05 -0400

    use chonkier runners
    Signed-off-by: William Woodruff <william@astral.sh>

--- plan-matrix.py
+++ plan-matrix.py
@@ -181,9 +181,9 @@ def main() -> None:
 
         # RUNNER: the GitHub Actions runner to use.
         if row["target-arch"] == "x86_64":
-            row["RUNNER"] = "depot-ubuntu-24.04-8"
+            row["RUNNER"] = "depot-ubuntu-24.04-16"
         elif row["target-arch"] == "aarch64":
-            row["RUNNER"] = "depot-ubuntu-24.04-arm-8"
+            row["RUNNER"] = "depot-ubuntu-24.04-arm-16"
         else:
             raise ValueError(f"Unknown target arch: {row['target-arch']}")
 
